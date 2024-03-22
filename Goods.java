a176e44f17333c55071adf86c80ffa96dd438edf
Наталя <ntlvitkova@gmail.com>
2024-03-22 12:30:07 +0200

    Додала клас Goods

---
+++ Goods.java
@@ -0,0 +1,13 @@
+public class Goods {
+    private int count; //кількість
+    private double price; // ціна
+    private char color; // колір "w" - білий, "b" - чорний, "r" - червоний
+}
+public double sum (int count, double price){ // метод підрахунку суми
+    double totalCost = count*price; //формула підрахунку
+    return totalCost; // повертає суму
+    }
+
+
+public static void main (String[] args){
+}
